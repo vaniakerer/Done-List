347351eb8dbc513b8593e4e593cf0bd7a71fb208
Kerer Ivan <vaniakerer8@gmail.com>
2016-11-28 00:08:13 +0200

    first commit

---
+++ app/src/main/java/donelist/lerndroid/com/donelist/CauseActivity.java
@@ -0,0 +1,13 @@
+package donelist.lerndroid.com.donelist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CauseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cause);
+    }
+}
