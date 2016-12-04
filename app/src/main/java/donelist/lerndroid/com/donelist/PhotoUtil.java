f6766e61d160b16896826555f2437e841ab3035c
Kerer Ivan <vaniakerer8@gmail.com>
2016-12-04 02:20:51 +0200

    add make photo (create new cause)

---
+++ app/src/main/java/donelist/lerndroid/com/donelist/PhotoUtil.java
@@ -0,0 +1,43 @@
+package donelist.lerndroid.com.donelist;
+
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by ivan on 04.12.16.
+ */
+
+public class PhotoUtil {
+
+
+    /** Create a File for saving an image or video */
+    public static Uri sss(){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
+
+        return Uri.fromFile(mediaFile);
+    }
+}
--- app/src/main/java/donelist/lerndroid/com/donelist/fragment/NewCauseFragment.java
+++ app/src/main/java/donelist/lerndroid/com/donelist/fragment/NewCauseFragment.java
@@ -1,7 +1,11 @@
 package donelist.lerndroid.com.donelist.fragment;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
@@ -9,9 +13,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Calendar;
 
 import butterknife.BindView;
@@ -29,16 +35,23 @@ import donelist.lerndroid.com.donelist.model.Cause;
 public class NewCauseFragment extends Fragment {
 
     private static final String TAG = "NewCauseFragment";
+
     private static final int REQUEST_MAKE_PHOTO = 0;
+
     private static final String EXTRA_CAUSE_ID = "donelist.lerndriod.com.donelist.cause_id";
 
     private File mPhotoFile;
 
-    @BindView(R.id.new_cause_title_ed) EditText mNewCauseTitleEd;
-    @BindView(R.id.new_cause_description_ed) EditText mNewCauseDescripptionEd;
-    @BindView(R.id.new_cause_edit_image_fab) FloatingActionButton mSubmitCauseFab;
+    @BindView(R.id.new_cause_title_ed)
+    EditText mNewCauseTitleEd;
+    @BindView(R.id.new_cause_description_ed)
+    EditText mNewCauseDescripptionEd;
+    @BindView(R.id.new_cause_photo_img)
+    ImageView mNewCausePhotoImg;
+    @BindView(R.id.new_cause_edit_image_fab)
+    FloatingActionButton mSubmitCauseFab;
 
-    public static NewCauseFragment newInstance(){
+    public static NewCauseFragment newInstance() {
         return new NewCauseFragment();
     }
 
@@ -51,13 +64,12 @@ public class NewCauseFragment extends Fragment {
         return v;
     }
 
-
     @OnClick(R.id.new_cause_edit_image_fab)
-    public void onFabClick(View view){
-        if (mNewCauseTitleEd.getText().toString().length() == 0){
+    public void onFabClick(View view) {
+        if (mNewCauseTitleEd.getText().toString().length() == 0) {
             Toast.makeText(getActivity(), R.string.must_be_filled, Toast.LENGTH_SHORT)
                     .show();
-        }else {
+        } else {
             CauseLab causeLab = CauseLab.get(getActivity());
             causeLab.addCause(bindCause());
 
@@ -69,7 +81,7 @@ public class NewCauseFragment extends Fragment {
         }
     }
 
-    private Cause bindCause(){
+    private Cause bindCause() {
         Cause cause = new Cause();
         Calendar c = Calendar.getInstance();
 
@@ -81,5 +93,37 @@ public class NewCauseFragment extends Fragment {
 
         return cause;
     }
+
+    @OnClick(R.id.new_cause_photo_img)
+    public void makePhotoListener() {
+        final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        boolean canTakePhoto = captureImage.resolveActivity(getActivity().getPackageManager()) != null;
+
+        if (canTakePhoto){
+            startActivityForResult(captureImage, REQUEST_MAKE_PHOTO);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK){
+            return;
+        }
+
+        switch (requestCode){
+            case REQUEST_MAKE_PHOTO:
+
+                Uri photoUri = data.getData();
+                try {
+                    Bitmap photo = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), photoUri);
+                    mNewCausePhotoImg.setImageBitmap(photo);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+    }
+
 
 }
