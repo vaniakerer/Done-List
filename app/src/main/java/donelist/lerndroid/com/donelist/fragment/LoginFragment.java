a5338e2c9b7222327ccefaf35862d78d7cf42e5b
Kerer Ivan <vaniakerer8@gmail.com>
2016-12-21 01:19:32 +0200

    fix animation

--- app/src/main/java/donelist/lerndroid/com/donelist/fragment/LoginFragment.java
+++ app/src/main/java/donelist/lerndroid/com/donelist/fragment/LoginFragment.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import donelist.lerndroid.com.donelist.R;
+import donelist.lerndroid.com.donelist.custom_views.AvenirBookEditText;
 
 /**
  * Created by ivan on 20.12.16.
@@ -31,6 +32,12 @@ public class LoginFragment extends Fragment {
     LinearLayout mLoginWithSocialLin;
     @BindView(R.id.fragment_login_sign_up_tv)
     TextView mSignUpTv;
+    @BindView(R.id.fragment_login_user_login)
+    AvenirBookEditText mUserLoginEd;
+    @BindView(R.id.fragment_login_password)
+    AvenirBookEditText mUserPasswordEd;
+
+    private boolean isLoginViewsVisible = true;
 
     public static final Fragment newInstance() {
         return new LoginFragment();
@@ -45,89 +52,111 @@ public class LoginFragment extends Fragment {
         return v;
     }
 
-    private void initListeners(){
+    private void initListeners() {
         //login views hide animation
-        final Animation hideLoginAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.anim_out_login_view);
-        final Animation showSocialAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.anim_in_social_view);
-
-        hideLoginAnimation.setFillAfter(true);
-        showSocialAnimation.setFillAfter(true);
-
-        hideLoginAnimation.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-                Log.d(TAG, "onAnimationStart hideLoginAnimation");
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-
-                Log.d(TAG, "onAnimationEnd hideLoginAnimation");
-                mLoginViewsLin.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
 
         mLoginWithSocialTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mLoginViewsLin.startAnimation(hideLoginAnimation);
-                mLoginWithSocialLin.startAnimation(showSocialAnimation);
-                mLoginWithSocialLin.setVisibility(View.VISIBLE);
+                animateViews();
             }
         });
 
         mSignUpTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final Animation showLoginViews = AnimationUtils.loadAnimation(getActivity(), R.anim.anim_in_login_view);
-                final Animation hideLoginWithSocialViews = AnimationUtils.loadAnimation(getActivity(), R.anim.anim_out_social_view);
-                showLoginViews.setFillAfter(true);
-                hideLoginWithSocialViews.setFillAfter(true);
 
-                showLoginViews.setAnimationListener(new Animation.AnimationListener() {
-                    @Override
-                    public void onAnimationStart(Animation animation) {
-                        mLoginViewsLin.setVisibility(View.VISIBLE);
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
-
-                    }
-                });
-
-                hideLoginWithSocialViews.setAnimationListener(new Animation.AnimationListener() {
-                    @Override
-                    public void onAnimationStart(Animation animation) {
-
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-                        mLoginWithSocialLin.setVisibility(View.GONE);
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
-
-                    }
-                });
-
-                mLoginViewsLin.startAnimation(showLoginViews);
-                mLoginWithSocialLin.startAnimation(hideLoginWithSocialViews);
             }
         });
     }
 
+
+    private void animateViews() {
+
+        isLoginViewsVisible = !isLoginViewsVisible;
+
+        if (!isLoginViewsVisible) {
+            mLoginWithSocialTv.setText(R.string.log_in_with_login_password_tv_text);
+            enableDisableViews(false);
+            final Animation hideLoginAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.anim_out_login_view);
+            final Animation showSocialAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.anim_in_social_view);
+
+            hideLoginAnimation.setFillAfter(true);
+            showSocialAnimation.setFillAfter(true);
+
+            hideLoginAnimation.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    Log.d(TAG, "onAnimationStart hideLoginAnimation");
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+
+                    Log.d(TAG, "onAnimationEnd hideLoginAnimation");
+                    mLoginViewsLin.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+            mLoginViewsLin.startAnimation(hideLoginAnimation);
+            mLoginWithSocialLin.startAnimation(showSocialAnimation);
+            mLoginWithSocialLin.setVisibility(View.VISIBLE);
+
+        } else {
+            mLoginWithSocialTv.setText(R.string.log_in_with_social_tv_text);
+            enableDisableViews(true);
+            final Animation showLoginViews = AnimationUtils.loadAnimation(getActivity(), R.anim.anim_in_login_view);
+            final Animation hideLoginWithSocialViews = AnimationUtils.loadAnimation(getActivity(), R.anim.anim_out_social_view);
+            showLoginViews.setFillAfter(true);
+            hideLoginWithSocialViews.setFillAfter(true);
+
+            showLoginViews.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    mLoginViewsLin.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+            hideLoginWithSocialViews.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    mLoginWithSocialLin.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+            mLoginViewsLin.startAnimation(showLoginViews);
+            mLoginWithSocialLin.startAnimation(hideLoginWithSocialViews);
+        }
+    }
+
+    private void enableDisableViews(boolean enabled) {
+        mUserLoginEd.setEnabled(enabled);
+        mUserPasswordEd.setEnabled(enabled);
+    }
 }
